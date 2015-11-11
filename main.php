403e79cb4caf12193a808b9501f651cd1a8a139f
Crystal Barton <atrus1701@gmail.com>
2015-11-11 06:26:02 -0500

    increment version.

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 Plugin Name: News Hub Sections
 Plugin URI: https://github.com/clas-web/news-hub-sections
 Description: 
-Version: 1.0.0
+Version: 1.1.0
 Author: Crystal Barton
 Author URI: https://www.linkedin.com/in/crystalbarton
 */
